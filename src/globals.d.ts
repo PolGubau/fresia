488814135b56c1b07fe6b31e09a9e9821b60b18d
Pol Gubau Amores <gubaupol@gmail.com>
2025-11-23 14:39:27 +0100

    Add new images for the hero section
    This commit introduces several new images to enhance the visual appeal of the hero section. The images include various decorations and elements that align with the current campaign theme. The image sources have been updated to reflect the new assets, ensuring a cohesive design.
    
    Issue: MW-0000
    Type: feature

---
+++ src/globals.d.ts
@@ -0,0 +1,21 @@
+declare module '*?lqip' {
+  const lqip: {
+    lqip: string;
+    width: number;
+    height: number;
+    src: string;
+  };
+  export default lqip;
+}
+// Vue mode
+declare module '*?lqip&vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+// React mode
+declare module '*?lqip&react' {
+  import type { FC, ImgHTMLAttributes } from 'react';
+  const component: FC<ImgHTMLAttributes<HTMLImageElement>>;
+  export default component;
+}
